64a670f4da18e79763cc51e606fb24b42ba4f175
Berkman Aydyn <68331365+Melonber@users.noreply.github.com>
2022-03-18 18:48:30 +0300

    Update App_2.js

--- CD2.3/App_2.js
+++ CD2.3/App_2.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView, ScrollView, StatusBar,Image,View,ImageBackground,Button } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, ScrollView, StatusBar,Image,View,ImageBackground,Button,Pressable,Alert } from 'react-native';
 import { color } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
-
+const image_bg = { uri: "https://i.pinimg.com/originals/9f/7e/82/9f7e8255a1c0b5f01c58de4666ea3a6b.gif" };
+const button_pressed = () => Alert.alert('Успешно !','Заявка подана')
 
 const App = () => {
+  function pressed(){
+    alert('Заявка подана !')
+  }
   return (
     <View style={styles.container}>
-      <Text style={{textAlign:'center', top:100, color:'#F0005F',fontFamily:'Apple Color Emoji',fontSize:15}}>Register to Rocket_Hackaton now</Text>
+      <ImageBackground source={image_bg} resizeMode="cover" style={styles.image}>
+      <Text style={{textAlign:'center', top:100, color:'#E88E05',fontFamily:'Georgia',fontSize:20, fontWeight:'bold'}}>Подай заявку на Rocket_Hackaton сейчас !</Text>
 
       <Image style={{ flex:1, left: 10,resizeMode:'contain', width:400, position:'relative'}} source={{
-          uri: 'https://cliply.co/wp-content/uploads/2019/03/371903250_ROCKET_LAUNCH_400px.gif'
+          uri: 'https://cdn.dribbble.com/users/418188/screenshots/3102257/rocket_animation_tubik_studio.gif'
           }}/>
-     <Button style={styles.button} title='Подать заявку'/>     
+      <Text style = {{bottom: 200, textAlign:'center', margin: 3, marginBottom: 20}}>Поучавствуй в хакатоне и поборись</Text>
+      <Text style = {{bottom: 200, textAlign:'center', margin: 3, marginBottom: 20}}>за главный приз: </Text> 
+      <Text style = {{bottom: 200, textAlign:'center', margin: 3, marginBottom: 20, color:'#10CA21'}}>1 000 000  ₽</Text>
+       
+     <Pressable style={styles.button} onPress={button_pressed}>
+      <Text style={styles.text}>Подать заявку</Text>
+    </Pressable>
+    </ImageBackground>
     </View>
   );
 }
@@ -24,14 +36,31 @@ const styles = StyleSheet.create({
     
   },
 
-  button:{
-    top:500,
-    color: 'red',
-    borderColor: 'black',
-    backgroundColor: 'purple',
-    borderWidth: 3
-  }
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 12,
+    borderRadius: 4,
+    elevation: 13,
+    backgroundColor: '#A70E4A',
+    bottom: 70,
+    width: 300,
+    left: 53
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    position: "relative",
 
+  },
 });
 
 export default App;
