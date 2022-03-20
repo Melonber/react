ec0b770ace99fd437a7120b335d72832efcd1515
Berkman Aydyn <68331365+Melonber@users.noreply.github.com>
2022-03-20 20:57:09 +0300

    Create DetailScreen.js

---
+++ CD2.4/screens/DetailScreen.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Button, View, Text, Pressable } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MessageScreen from './Messages';
+
+function DetailsScreen({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go Message screen"
+          onPress={() => navigation.navigate('Messages')}
+        />
+        <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+        <Button title="Go back" onPress={() => navigation.goBack()} />
+      </View>
+    );
+  }
+
+  export default DetailsScreen
