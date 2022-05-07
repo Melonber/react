751d773aad985e80070a45e30736c2e17fc7f403
Беркман Айдын <68331365+Melonber@users.noreply.github.com>
2022-05-07 21:22:18 +0300

    Update App.js

--- CD2.7/App.js
+++ CD2.7/App.js
@@ -1,1 +1,29 @@
+import { StyleSheet, Text, View,Button } from 'react-native'
+import React from 'react'
+import {useState} from 'react'
+const App = () => {
 
+  const [count, setCount] = useState(0)
+
+  return (
+    <View style={styles.text}>
+      <Text style={styles.text}>Работает</Text>
+      <Button title="Кнопка" onPress={()=>setCount(count+1)}/>
+      <Text style={{textAlign:'center'}}>{count}</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  text: {
+    textAlign: 'center',
+    justifyContent:'center',
+    alignContent:'center',
+    fontSize: 20,
+    color: 'red',
+    top:200
+    
+  }
+})
