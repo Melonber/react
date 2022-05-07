f67aff69c6c4f0c03a9e6a252a941cd499b630f7
Беркман Айдын <68331365+Melonber@users.noreply.github.com>
2022-05-07 21:10:13 +0300

    Update App.js

--- CD2.6/App.js
+++ CD2.6/App.js
@@ -1,1 +1,26 @@
+ import React from 'react';
+ import {
+   SafeAreaView,
+   Text,
+ } from 'react-native';
+import Main from './Main';
+import Login from './Login'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
+
+ const App = () => {
+ 
+   return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown:false}}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Main" component={Main} />
+      </Stack.Navigator>
+    </NavigationContainer>
+   );
+ };
+ 
+ 
+ export default App;
